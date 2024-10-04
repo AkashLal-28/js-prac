363e0042cfb860b24cd4a3643d191b794d9c8828
AkashLal-28 <113417847+AkashLal-28@users.noreply.github.com>
2024-10-04 10:50:07 +0000

    datatypes done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "akash"  //string
+
+let num = 2  //int
+
+let married = true  //boolean
+
+let temperature = null   //object
+let person = {
+    firstName: "Akash",
+    lastName: "Lal"
+};
+
+let cars = ["BM", "Suzu", "ferr"]   //Array object
+
+let state;   //undefined
